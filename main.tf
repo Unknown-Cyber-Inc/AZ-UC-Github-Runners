fb98247c1fa1e2e4d181ff1662cb6072a4fae3c4
uc-justinconger <140862386+uc-justinconger@users.noreply.github.com>
2023-08-29 20:04:43 -0500

    update code

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "example" {
+  name = "virusbattle-gitlab-runners"
+}
+
+data "azurerm_subnet" "example" {
+  name                 = "Github-runners"
+  virtual_network_name = "production-vnet"
+  resource_group_name  = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_container_group" "example" {
+  name                = "aci-uc-github-runners"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  ip_address_type     = "Private"
+  subnet_ids          = data.azurerm_subnet.example.id
+  os_type             = "Linux"
+
+  
+  container {
+    name   = "uc-custinfra-github-runner"
+    image  = "myoung34/github-runner"
+    cpu    = "0.5"
+    memory = "1.5"
+    environment_variables = {
+        "RUNNER_NAME"  = uc-cust-github-runner
+        "RUNNER_TOKEN" = BBSWHMSMOVOGCCL6CEG62JDE5VO4O
+        "ORG_NAME"     = Unknown-Cyber-Inc
+        "REPO_URL"     = "https://github.com/Unknown-Cyber-Inc/AZ-UC-Cust-Infra"
+    }
+  }
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  
+  tags = {
+    environment = "testing"
+  }
+}
