f436e38a7c4836bddc17a0dbf596968e98a5fee6
Shannon Ford <shannon.ford@unknowncyber.com>
2023-09-19 10:02:33 -0500

    fixced var

--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 variable "containers" {
   type = map(object({
-    acg_name     = string
-    dockerimage  = string
-    runner_name  = string
-    runner_token = string
-    repo_url     = string
+    acg_name      = string
+    docker_image  = string
+    runner_name   = string
+    runner_token  = string
+    repo_url      = string
   }))
   default = {
     "core" = {
       acg_name     = "acg-uc-core-github-runners"
-      image        = "myoung34/github-runner"
+      docker_image = "myoung34/github-runner"
       runner_name  = "az-uc-core-infra-github-runner"
       runner_token = "BBSTPEKTEJ2BKFB2YR6I4ADFBG7OQ"
       repo_url     = "https://github.com/Unknown-Cyber-Inc/AZ-UC-Core-Infra"
     }
     "cust" = {
-      image        = "myoung34/github-runner"
+      acg_name     = "acg-uc-cust-github-runners"
+      docker_image = "myoung34/github-runner"
       runner_name  = "az-uc-core-infra-github-runner"
       runner_token = "BBSTPEKTEJ2BKFB2YR6I4ADFBG7OQ"
       repo_url     = "https://github.com/Unknown-Cyber-Inc/AZ-UC-Core-Infra"
