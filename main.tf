4b44768cb47a3032a9ef7361a479d83a08f9364b
Shannon Ford <shannon.ford@unknowncyber.com>
2023-10-06 12:13:11 -0500

    update

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ data "azurerm_subnet" "ucgithubrunnersubnet" {
 }
 
 resource "azurerm_container_group" "ucacg" {
-  for_each            = var.containers
-  name                = each.value.acg_name
+  for_each            = toset(["infra","cust"])
+  name                = var.az_acg_name
   location            = data.azurerm_resource_group.ucgithubrunnerrg.location
   resource_group_name = data.azurerm_resource_group.ucgithubrunnerrg.name
   ip_address_type     = "Private"
@@ -40,15 +40,17 @@ resource "azurerm_container_group" "ucacg" {
   os_type             = "Linux"
 
   container {
-    name   = each.value.runner_name
-    image  = each.value.docker_image
+    name   = join("-","az-into365exch",each.key,"github-runner")
+    image  = var.aci_docker_image
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-        "RUNNER_NAME"  = each.value.runner_name
-        "RUNNER_TOKEN" = each.key == "infra" ? var.az_into365exch_infra_github_runner_token : var.az_into365exch_cust_github_runner_token
-        "ORG_NAME"     = "Unknown-Cyber-Inc"
-        "REPO_URL"     = each.value.repo_url
+        "RUNNER_NAME"  = join("-","az-into365exch",each.key,"github-runner")
+        "RUNNER_TOKEN" = var.az_into365exch_infra_github_runner_token
+        "ORG_NAME"     = var.gh_runner_org_name
+        "RUNNER_GROUP" = var.gh_runner_group_name
+        "RUNNER_SCOPE" = var.gh_runner_scope
+        "LABELS"       = each.key
     }
   
     ports {
@@ -60,3 +62,16 @@ resource "azurerm_container_group" "ucacg" {
     environment = "testing"
   }
 }
+
+
+docker run -d --restart always --name github-runner \
+  -e RUNNER_NAME_PREFIX="myrunner" \
+  -e ACCESS_TOKEN="footoken" \
+  -e RUNNER_WORKDIR="/tmp/github-runner-your-repo" \
+  -e RUNNER_GROUP="my-group" \
+  -e RUNNER_SCOPE="org" \
+  -e DISABLE_AUTO_UPDATE="true" \
+  -e ORG_NAME="octokode" \
+  -e LABELS="my-label,other-label" \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /tmp/github-runner-your-repo:/tmp/github-runner-your-repo \
--- variables.tf
+++ variables.tf
@@ -6,25 +6,50 @@ variable "az_into365exch_cust_github_runner_token" {
     description = "az-into365exch-cust Repo Self Hosted Runner Token"
 }
 
-variable "containers" {
-  type = map(object({
-    acg_name      = string
-    docker_image  = string
-    runner_name   = string
-    repo_url      = string
-  }))
-  default = {
-    "infra" = {
-      acg_name     = "acg-into365exch-infra-github-runners"
-      docker_image = "myoung34/github-runner"
-      runner_name  = "az-into365exch-infra-github-runner"
-      repo_url     = "https://github.com/Unknown-Cyber-Inc/az-into365exch-infra"
-    }
-    "cust" = {
-      acg_name     = "acg-into365exch-cust-github-runners"
-      docker_image = "myoung34/github-runner"
-      runner_name  = "az-into365exch-cust-github-runner"
-      repo_url     = "https://github.com/Unknown-Cyber-Inc/az-into365exch-cust"
-    }
-  }
+#variable "containers" {
+#  type = map(object({
+#    acg_name      = string
+#    docker_image  = string
+#    runner_name   = string
+#    repo_url      = string
+#  }))
+#  default = {
+#    "infra" = {
+#      acg_name     = "acg-into365exch-infra-github-runners"
+#      docker_image = "myoung34/github-runner"
+#      runner_name  = "az-into365exch-infra-github-runner"
+#      repo_url     = "https://github.com/Unknown-Cyber-Inc/az-into365exch-infra"
+#    }
+#    "cust" = {
+#      acg_name     = "acg-into365exch-cust-github-runners"
+#      docker_image = "myoung34/github-runner"
+#      runner_name  = "az-into365exch-cust-github-runner"
+#      repo_url     = "https://github.com/Unknown-Cyber-Inc/az-into365exch-cust"
+#    }
+#  }
+#}
+
+variable "az_acg_name" {
+    description = "The Name Of The Azure Container Group"
+    default = "acg-into365exch-infra-github-runners"
+}
+
+variable "aci_docker_image" {
+    description = "The Name Of The Image To Use For The Containers"
+    default = "myoung34/github-runner"
+}
+
+variable "gh_runner_group_name" {
+    description = "The Name Of The Runner Group To Join The Runner To In Github"
+    default = "into365exch-runners"
+}
+
+variable "gh_runner_scope" {
+    description = "The Scope Of The Runner"
+    default = "org"
+}
+
+variable "gh_runner_org_name" {
+    description = "The Name Of The Organization In Github For The Runner"
+    default = "Unknown-Cyber-Inc"
 }
