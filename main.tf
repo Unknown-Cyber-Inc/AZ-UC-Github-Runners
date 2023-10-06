ed339ca573e1fb479e0f745752455333d6dbdc70
Shannon Ford <shannon.ford@unknowncyber.com>
2023-10-06 12:55:17 -0500

    for each

--- main.tf
+++ main.tf
@@ -31,7 +31,6 @@ data "azurerm_subnet" "ucgithubrunnersubnet" {
 }
 
 resource "azurerm_container_group" "ucacg" {
-  for_each            = toset(["infra","cust"])
   name                = var.az_acg_name
   location            = data.azurerm_resource_group.ucgithubrunnerrg.location
   resource_group_name = data.azurerm_resource_group.ucgithubrunnerrg.name
@@ -39,26 +38,32 @@ resource "azurerm_container_group" "ucacg" {
   subnet_ids          = [data.azurerm_subnet.ucgithubrunnersubnet.id]
   os_type             = "Linux"
 
-  container {
-    name   = join("-",["az-into365exch",each.key,"github-runner"])
-    image  = var.aci_docker_image
-    cpu    = "0.5"
-    memory = "1.5"
-    environment_variables = {
+  #Infra Container
+  dynamic container {
+    for_each = toset(["infra","cust"])
+    content {
+      name   = join("-",["az-into365exch",each.key,"github-runner"])
+      image  = var.aci_docker_image
+      cpu    = "0.5"
+      memory = "1.5"
+      environment_variables = {
         "RUNNER_NAME"  = join("-",["az-into365exch",each.key,"github-runner"])
         "RUNNER_TOKEN" = var.az_into365exch_infra_github_runner_token
         "ORG_NAME"     = var.gh_runner_org_name
         "RUNNER_GROUP" = var.gh_runner_group_name
         "RUNNER_SCOPE" = var.gh_runner_scope
         "LABELS"       = each.key
-    }
+      }
   
-    ports {
-      port     = 443
-      protocol = "TCP"
-    }
+      ports {
+        port     = 443
+        protocol = "TCP"
+      }
+    }  
   }
+
   tags = {
     environment = "testing"
   }
 }
+
