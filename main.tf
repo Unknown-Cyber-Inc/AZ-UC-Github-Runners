8f0432d2b188b0f0eef0ff6281e2bac00519b365
Shannon Ford <shannon.ford@unknowncyber.com>
2023-10-06 12:16:05 -0500

    syntax

--- main.tf
+++ main.tf
@@ -62,16 +62,3 @@ resource "azurerm_container_group" "ucacg" {
     environment = "testing"
   }
 }
-
-
-docker run -d --restart always --name github-runner \
-  -e RUNNER_NAME_PREFIX="myrunner" \
-  -e ACCESS_TOKEN="footoken" \
-  -e RUNNER_WORKDIR="/tmp/github-runner-your-repo" \
-  -e RUNNER_GROUP="my-group" \
-  -e RUNNER_SCOPE="org" \
-  -e DISABLE_AUTO_UPDATE="true" \
-  -e ORG_NAME="octokode" \
-  -e LABELS="my-label,other-label" \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /tmp/github-runner-your-repo:/tmp/github-runner-your-repo \
