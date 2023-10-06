6b0c1fc231ae87bee0108c34228cad451ce7dac1
Shannon Ford <shannon.ford@unknowncyber.com>
2023-10-06 13:01:41 -0500

    change

--- main.tf
+++ main.tf
@@ -38,32 +38,50 @@ resource "azurerm_container_group" "ucacg" {
   subnet_ids          = [data.azurerm_subnet.ucgithubrunnersubnet.id]
   os_type             = "Linux"
 
-  #Infra Container
-  dynamic container {
-    for_each = toset(["infra","cust"])
-    content {
-      name   = join("-",["az-into365exch",each.key,"github-runner"])
-      image  = var.aci_docker_image
-      cpu    = "0.5"
-      memory = "1.5"
-      environment_variables = {
-        "RUNNER_NAME"  = join("-",["az-into365exch",each.key,"github-runner"])
+  #Infra
+  container {
+    name   = "az-into365exch-infra-github-runner"
+    image  = var.aci_docker_image
+    cpu    = "0.5"
+    memory = "1.5"
+    environment_variables = {
+        "RUNNER_NAME"  = "az-into365exch-infra-github-runner"
         "RUNNER_TOKEN" = var.az_into365exch_infra_github_runner_token
         "ORG_NAME"     = var.gh_runner_org_name
         "RUNNER_GROUP" = var.gh_runner_group_name
         "RUNNER_SCOPE" = var.gh_runner_scope
-        "LABELS"       = each.key
-      }
+        "LABELS"       = "infra"
+    }
   
-      ports {
-        port     = 443
-        protocol = "TCP"
-      }
-    }  
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
   }
 
+  #Customer
+  container {
+    name   = "az-into365exch-cust-github-runner"
+    image  = var.aci_docker_image
+    cpu    = "0.5"
+    memory = "1.5"
+    environment_variables = {
+        "RUNNER_NAME"  = "az-into365exch-cust-github-runner"
+        "RUNNER_TOKEN" = var.az_into365exch_infra_github_runner_token
+        "ORG_NAME"     = var.gh_runner_org_name
+        "RUNNER_GROUP" = var.gh_runner_group_name
+        "RUNNER_SCOPE" = var.gh_runner_scope
+        "LABELS"       = "cust"
+    }
+  
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+  }
+
+  
   tags = {
     environment = "testing"
   }
 }
-
